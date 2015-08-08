2e36478b68eec88f45a846f8d54c8ac6747585ec
Akshay Chiwhane <achiwhane@gmail.com>
2015-08-07 23:48:59 -0400

    add refresh

--- ReadHN/HackerNewsBrain.swift
+++ ReadHN/HackerNewsBrain.swift
@@ -61,46 +61,5 @@ class HackerNewsBrain {
     
     // we want this to return nil in the event that we don't get a valid ID or
     // if the item gets deleted
-    func generateStoryFromID(id: Int, storyIndex: Int, callback: () -> ()){
-        println("\(storyIndex)")
-        let url = NSURL(string: "https://hacker-news.firebaseio.com/v0/item/\(id).json")
-        if let storyUrl = url {
-            // create an empty story
-            // tart an async session to retrieve and parse json object for story
-            let queue = dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)
-            dispatch_async(queue) {
-                var session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
-                let jsonRequest = session.dataTaskWithURL(storyUrl, completionHandler: { (data, response, error) -> Void in
-                    if error != nil {
-                        println("\(error.localizedDescription)")
-                    }
-                    
-                    var parseError: NSError? // just in case
-                    var jsonData: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &parseError)
-                    
-                    if parseError != nil {
-                        println("\(parseError?.localizedDescription)")
-                    }
-                    
-                    if let storyJson = jsonData as! NSDictionary? {
-                        // set the story title and url in defaults
-                        if let title = storyJson["title"] as! String? {
-                            self.defaults.setObject(title, forKey: "\(storyIndex).title")
-                        }
-                        if let url = storyJson["url"] as! String? {
-                            self.defaults.setObject(url, forKey:"\(storyIndex).url")
-                        }
-                        if let score = storyJson["score"] as! Int?{
-                            self.defaults.setObject(score, forKey: "\(storyIndex).score")
-                        }
-                        if let by = storyJson["by"] as? String {
-                            self.defaults.setObject(by, forKey: "\(storyIndex).by")
-                        }
-                    }
-                    println("loaded data for item number: \(storyIndex)")
-                })
-                jsonRequest.resume()
-            }
-        }
-    }
+
 }
--- ReadHN/StoriesTableViewController.swift
+++ ReadHN/StoriesTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class StoriesTableViewController: UITableViewController {
+class StoriesTableViewController: UITableViewController, UITableViewDelegate, UITableViewDataSource {
     
     let numberStories = 20
     let defaults = NSUserDefaults.standardUserDefaults()
@@ -18,12 +18,14 @@ class StoriesTableViewController: UITableViewController {
     struct Key {
         static let sID: String = "submissionids.array"
     }
-
-
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initRefreshControl()
+        refresh()
+    }
+    
+    func refresh() {
         brain.startConnection() {
             let storyIDs = self.defaults.objectForKey(Key.sID) as? [Int] ?? []
             println("\(storyIDs.count)")
@@ -31,16 +33,86 @@ class StoriesTableViewController: UITableViewController {
                 var count = 20
                 var i = 0
                 while (i < count){
-                    self.brain.generateStoryFromID(storyIDs[i], storyIndex: i) {
-                        dispatch_async(dispatch_get_main_queue(),{
-                            self.tableView.reloadData()
-                        })
-                    }
+                    self.generateStoryFromID(storyIDs[i], storyIndex: i)
                     i++
                 }
             }
         }
+        self.tableView.reloadData()
+        self.refreshControl?.endRefreshing()
+    }
     
+    func initRefreshControl() {
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl?.addTarget(self, action: "refresh", forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    func generateStoryFromID(id: Int, storyIndex: Int){
+        println("\(storyIndex)")
+        let url = NSURL(string: "https://hacker-news.firebaseio.com/v0/item/\(id).json")
+        if let storyUrl = url {
+            // create an empty story
+            // tart an async session to retrieve and parse json object for story
+            let queue = dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)
+            dispatch_async(queue) {
+                var session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+                let jsonRequest = session.dataTaskWithURL(storyUrl, completionHandler: { (data, response, error) -> Void in
+                    if error != nil {
+                        println("\(error.localizedDescription)")
+                    }
+                    
+                    var parseError: NSError? // just in case
+                    var jsonData: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &parseError)
+                    
+                    if parseError != nil {
+                        println("\(parseError?.localizedDescription)")
+                    }
+                    
+                    if let storyJson = jsonData as! NSDictionary? {
+                        // set the story title and url in defaults
+                        if let title = storyJson["title"] as! String? {
+                            self.defaults.setObject(title, forKey: "\(storyIndex).title")
+                        }
+                        if let url = storyJson["url"] as! String? {
+                            self.defaults.setObject(url, forKey:"\(storyIndex).url")
+                        }
+                        if let score = storyJson["score"] as! Int?{
+                            self.defaults.setObject(score, forKey: "\(storyIndex).score")
+                        }
+                        if let by = storyJson["by"] as? String {
+                            self.defaults.setObject(by, forKey: "\(storyIndex).by")
+                        }
+                    }
+                    println("loaded data for item number: \(storyIndex)")
+                    
+                    dispatch_async(dispatch_get_main_queue()) {
+                        
+                        if let cell = self.tableView.cellForRowAtIndexPath(NSIndexPath(forRow: storyIndex, inSection: 0)) {
+                            self.formatCell(cell, atRow: storyIndex)
+                        }
+                        self.tableView.reloadData()
+                    }
+                })
+                jsonRequest.resume()
+            }
+        }
+    }
+    
+    private func formatCell(cell: UITableViewCell, atRow row: Int) {
+        if let title = self.defaults.objectForKey("\(row).title") as? String {
+            cell.textLabel?.text = title
+            cell.textLabel?.font = UIFont.boldSystemFontOfSize(CGFloat(16.0))
+            
+            var tempStr = ""
+            if let score = self.defaults.objectForKey("\(row).score") as? Int {
+                tempStr += "\(score) point(s) | "
+            }
+            if let user = self.defaults.objectForKey("\(row).by") as? String {
+                tempStr += "by \(user)"
+            }
+            
+            cell.detailTextLabel?.text = tempStr
+        }
     }
     
 
@@ -69,25 +141,7 @@ class StoriesTableViewController: UITableViewController {
         let dequeued: AnyObject = tableView.dequeueReusableCellWithIdentifier("storyCell", forIndexPath: indexPath)
         let cell = dequeued as! UITableViewCell
         
-        if let title = defaults.objectForKey("\(indexPath.row).title") as? String {
-            cell.textLabel?.text = title
-            cell.textLabel?.font = UIFont.boldSystemFontOfSize(CGFloat(16.0))
-            
-            var tempStr = ""
-            if let score = defaults.objectForKey("\(indexPath.row).score") as? Int {
-                tempStr += "\(score) point(s) | "
-            }
-            if let user = defaults.objectForKey("\(indexPath.row).by") as? String {
-                tempStr += "by \(user)"
-            }
-            
-            cell.detailTextLabel?.text = tempStr
-
-        } else {
-            var title = defaults.objectForKey("\(indexPath.row).title") as? String ?? ""
-            println("current title: \(title)")
-            println("intended title: \(indexPath.row).title")
-        }
+        formatCell(cell, atRow: indexPath.row)
         
         return cell
     }
